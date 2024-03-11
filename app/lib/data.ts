e6f853b0e3b6619a9a1f3e7a4f70669febf02e13
GiadaGiuliaPaola <giadatamassia@gmail.com>
2024-03-11 09:20:01 +0100

    add sql

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -52,6 +52,8 @@ export async function fetchLatestInvoices() {
   }
 }
 
+
+// PARALLEL DATA FETCHING
 export async function fetchCardData() {
   try {
     // You can probably combine these into a single SQL query
